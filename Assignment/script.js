44853e4d8569713da3d75fb6e7c1505bc270542f
pkit24aamn-star <pkit24a.amn@ismt.edu.np>
2025-08-28 08:32:27 +0545

    Add files via upload

---
+++ Assignment/script.js
@@ -0,0 +1,7 @@
+//    A button that calls a JavaScript function 
+  <button onclick="showMessage()">Reveal Magic</button>
+
+  
+    function showMessage() {
+      alert("✨ Welcome to the Wizarding World of Harry Potter! ✨");
+    }
